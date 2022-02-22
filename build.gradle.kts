9e077217bc815bb68ac1f0f761467224c29ba8ad
Sergey.Moryahin <123>
2022-02-22 17:53:43 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.20-M1"
+    application
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(files("libs/lib.jar"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.freeCompilerArgs += listOf("-Xskip-prerelease-check")
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+import contextReceiversLib.LibraryClassOne
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+    val libOne = LibraryClassOne()
+
+//    libOne.test()
+}
